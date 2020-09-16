576dc54ed47acead98ee7749555a48aef9e306fd
Holmze <704062177@qq.com>
2020-09-16 19:35:57 +0800

    update code and codestyle

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -2,13 +2,24 @@ import json
 import os
 import argparse
 import time
+import mmap
+from multiprocessing import Pool, cpu_count
+import shutil
 
 class Data:
-    def __init__(self, dict_address: int = None, reload: int = 0):
+    def __init__(self, dict_address: str = None, reload: int = 0):
         if reload == 1:
+            try:
+                os.makedirs('json_temp')
+            except:
+                shutil.rmtree('json_temp')
+                os.makedirs('json_temp')
             self.__init(dict_address)
-        if dict_address is None and not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists('3.json'):
-            raise RuntimeError('error: init failed')
+
+        if dict_address is None and \
+                not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists('3.json'):
+            raise RuntimeError("Error: init failed")
+
         x = open('1.json', 'r', encoding='utf-8').read()
         self.__4Events4PerP = json.loads(x)
         x = open('2.json', 'r', encoding='utf-8').read()
@@ -18,65 +29,76 @@ class Data:
 
     def __init(self, dict_address: str):
         start_time = time.time()
-        json_list = []
-        for root, dic, files in os.walk(dict_address):## 获取文件目录下所有json文件
-            for f in files:
-                if f[-5:] == '.json':
-                    json_path = f
-                    x = open(dict_address+'\\'+json_path,'r', encoding='utf-8').read()
-                    str_list = [_x for _x in x.split('\n') if len(_x) > 0]
-                    for i, _str in enumerate(str_list):
-                        try:
-                            json_list.append(json.loads(_str))
-                        except:
-                            pass
-        # print("read file:",time.time()-start_time)
-        # print(json_list[0])
-        # print(json_list[1])
-        # print(json_list[2])
-        # records = self.__listOfNestedDict2ListOfDict(json_list)
-        # print("list Of Nested Dict to List of Dict:",time.time()-start_time)
-        # print(records[0])
         self.__4Events4PerP = {}
         self.__4Events4PerR = {}
         self.__4Events4PerPPerR = {}
-        # print(type(records))
-        for i in json_list:
-            if not self.__4Events4PerP.get(i['actor']['login'], 0):
-                self.__4Events4PerP.update({i['actor']['login']: {}})
-                self.__4Events4PerPPerR.update({i['actor']['login']: {}})
-            self.__4Events4PerP[i['actor']['login']][i['type']] = self.__4Events4PerP[i['actor']['login']].get(i['type'], 0)+1
-            if not self.__4Events4PerR.get(i['repo']['name'], 0):
-                self.__4Events4PerR.update({i['repo']['name']: {}})
-            self.__4Events4PerR[i['repo']['name']][i['type']] = self.__4Events4PerR[i['repo']['name']].get(i['type'], 0)+1
-            if not self.__4Events4PerPPerR[i['actor']['login']].get(i['repo']['name'], 0):
-                self.__4Events4PerPPerR[i['actor']['login']].update({i['repo']['name']: {}})
-            self.__4Events4PerPPerR[i['actor']['login']][i['repo']['name']][i['type']] = self.__4Events4PerPPerR[i['actor']['login']][i['repo']['name']].get(i['type'], 0)+1
-        # print("count:",time.time()-start_time)
+
+        for root, dic, files in os.walk(dict_address):
+            pool = Pool(processes=max(cpu_count(), 6))
+            for f in files:
+                if f[-5:] == '.json':
+                    pool.apply_async(self.readFile, args=(f, dict_address))
+            pool.close()
+            pool.join()
+        print("read file:",time.time()-start_time)
+
+        for root, dic, files in os.walk("json_temp"):
+            for f in files:
+                if f[-5:] == '.json':
+                    json_list = json.loads(open("json_temp" + '\\' + f, 'r', encoding='utf-8').read())
+                    for item in json_list:
+                        if not self.__4Events4PerP.get(item['actor__login'], 0):
+                            self.__4Events4PerP.update({item['actor__login']: {}})
+                            self.__4Events4PerPPerR.update({item['actor__login']: {}})
+                        self.__4Events4PerP[item['actor__login']][item['type']] = \
+                            self.__4Events4PerP[item['actor__login']].get(item['type'], 0) + 1
+
+                        if not self.__4Events4PerR.get(item['repo__name'], 0):
+                            self.__4Events4PerR.update({item['repo__name']: {}})
+                        self.__4Events4PerR[item['repo__name']][item['type']] = \
+                            self.__4Events4PerR[item['repo__name']].get(item['type'], 0) + 1
+
+                        if not self.__4Events4PerPPerR[item['actor__login']].get(item['repo__name'], 0):
+                            self.__4Events4PerPPerR[item['actor__login']].update({item['repo__name']: {}})
+                        self.__4Events4PerPPerR[item['actor__login']][item['repo__name']][item['type']] = \
+                            self.__4Events4PerPPerR[item['actor__login']][item['repo__name']].get(item['type'], 0) + 1
+        print("count:",time.time()-start_time)
         with open('1.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerP,f)
+            json.dump(self.__4Events4PerP, f)
         with open('2.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerR,f)
+            json.dump(self.__4Events4PerR, f)
         with open('3.json', 'w', encoding='utf-8') as f:
-            json.dump(self.__4Events4PerPPerR,f)
-        # print("write",time.time()-start_time)
+            json.dump(self.__4Events4PerPPerR, f)
+        print("write file:",time.time()-start_time)
 
-    def __parseDict(self, d: dict, prefix: str):
-        _d = {}
-        for k in d.keys():
-            if str(type(d[k]))[-6:-2] == 'dict':
-                _d.update(self.__parseDict(d[k], k))
-            else:
-                _k = f'{prefix}__{k}' if prefix != '' else k
-                _d[_k] = d[k]
-        return _d
 
-    def __listOfNestedDict2ListOfDict(self, a: list):
-        records = []
-        for d in a:
-            _d = self.__parseDict(d, '')
-            records.append(_d)
-        return records
+    def saveJson(self, json_list, filename):
+        batch_message = []
+        for item in json_list:
+            if item['type'] not in ["PushEvent", "IssueCommentEvent", "IssuesEvent", "PullRequestEvent"]:
+                continue
+            batch_message.append({'actor__login': item['actor']['login'], 'type': item['type'], 'repo__name': item['repo']['name']})
+        with open('json_temp\\' + filename, 'w', encoding='utf-8') as F:
+            json.dump(batch_message, F)
+
+
+    def readFile(self, f, dict_address):
+        json_list = []
+        if f[-5:] == '.json':
+            json_path = f
+            x = open(dict_address + '\\' + json_path, 'r', encoding='utf-8')
+            with mmap.mmap(x.fileno(), 0, access=mmap.ACCESS_READ) as m:
+                m.seek(0, 0)
+                obj = m.read()
+                obj = str(obj, encoding="utf-8")
+                str_list = [_x for _x in obj.split('\n') if len(_x) > 0]
+                for _str in str_list:
+                    try:
+                        json_list.append(json.loads(_str))
+                    except:
+                        pass
+            self.saveJson(json_list, f)
+
 
     def getEventsUsers(self, username: str, event: str) -> int:
         if not self.__4Events4PerP.get(username,0):
@@ -104,7 +126,9 @@ class Run:
         self.parser = argparse.ArgumentParser()
         self.data = None
         self.argInit()
+        t1 = time.time()
         print(self.analyse())
+        t2 = time.time()
 
     def argInit(self):
         self.parser.add_argument('-i', '--init')
@@ -112,7 +136,7 @@ class Run:
         self.parser.add_argument('-r', '--repo')
         self.parser.add_argument('-e', '--event')
 
-    def analyse(self):#解析命令
+    def analyse(self):
         if self.parser.parse_args().init:
             self.data = Data(self.parser.parse_args().init, 1)
             return 0
@@ -122,17 +146,19 @@ class Run:
             if self.parser.parse_args().event:
                 if self.parser.parse_args().user:
                     if self.parser.parse_args().repo:
-                        res = self.data.getEventsUsersAndRepos(self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
+                        res = self.data.getEventsUsersAndRepos(
+                            self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
                     else:
-                        res = self.data.getEventsUsers(self.parser.parse_args().user, self.parser.parse_args().event)
+                        res = self.data.getEventsUsers(
+                            self.parser.parse_args().user, self.parser.parse_args().event)
                 elif self.parser.parse_args().repo:
-                    res = self.data.getEventsRepos(self.parser.parse_args().repo, self.parser.parse_args().event)
+                    res = self.data.getEventsRepos(
+                        self.parser.parse_args().repo, self.parser.parse_args().event)
                 else:
-                    raise RuntimeError('error: argument -l or -c are required')
+                    raise RuntimeError("Error: argument -u|--user or -r|--repo cannot be found!")
             else:
-                raise RuntimeError('error: argument -e is required')
+                raise RuntimeError("Error: argument -e|--event cannot be found!")
         return res
-
 
 if __name__ == '__main__':
     a = Run()
